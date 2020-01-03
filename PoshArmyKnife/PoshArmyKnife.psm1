0fcbb22efd613a8ac10bbe01a042fb2dd0a54156
chrisrowe851 <59376396+chrisrowe851@users.noreply.github.com>
2020-01-03 11:18:47 +1000

    Lots of changes

--- PoshArmyKnife/PoshArmyKnife.psm1
+++ PoshArmyKnife/PoshArmyKnife.psm1
@@ -3,27 +3,11 @@ Collection of useful functions
 
 https://github.com/chrisrowe851/justposhthings
 
+Requires PSFramework module installed
+
 #>
 
-#common root directory for all functions
-$rootdir="C:\PoshArmyKnife"
-$filesdir
-$logsdir
-
-
-Function Initialize-PoshArmyKnife{
-    #creates common local resources needed for functions
-
-    [cmdletbinding(SupportsShouldProcess=$True)]
-
-#
-param()
-
-$script:logsdir = New-Item -ItemType Directory -Path "$rootdir\logs" -Force
-$script:filesdir = New-Item -ItemType Directory -Path "$rootdir\files" -Force
-}
-
-Function Remove-OldSnapshots
+Function Remove-OldSnapshotsPAK
 {
 
 <#
@@ -31,13 +15,13 @@ Function Remove-OldSnapshots
     Delete snapshots older than "x" days from VMs provided in CSV file. Works across multiple vCenters
 
 .DESCRIPTION
-    As per synopsis
+    You must provide either vCenter name or CSV file, but not both. If vCenter name is used all VMs attached to that vCenter will be targeted
 
 .PARAMETER Grace
     The number of days grace to give snapshots (eg do not delete snapshots if created within <grace> days)
 
 .PARAMETER CredLabel
-    This script uses windows credential manager credentials. You must provide the name/label of the credential you have created and wish to use to connect to all vCenters. There is no support for vCenters with different credentials at this time
+    This function uses windows credential manager credentials. You must provide the name/label of the credential you have created and wish to use to connect to all vCenters. There is no support for vCenters with different credentials at this time
 
 .PARAMETER CSVFile
     The location of the csv file containing VM list. Must be formatted with 2 headers [VCENTER] and [VM]
@@ -68,90 +52,58 @@ param
 (
     [Parameter(Mandatory=$true)] [int32] $Grace, #number of grace days for snapshots
     [Parameter(Mandatory=$true)] [string] $CredLabel, #stored credentials from windows cred store to use
-    [string] $csvfile ="$filesdir\Remove-OldSnapshots-VMList.csv", # CSV file where VM list is found, must exist
-    [string] $logfile ="$logsdir\Remove-OldSnapshots-Log.txt", # logfile location, will be created if not exist
-    [Parameter(Mandatory=$true)] [string] $SnapSuffix
+    [Parameter(Mandatory=$false)] $csvfile, # CSV file where VM list is found, must exist
+    [Parameter(Mandatory=$false)] $vcenter, #If no CSV is provided, a vCenter must be specified
+    [Parameter(Mandatory=$true)] [string] $SnapSuffix #Suffix that is attached to snapshots for deletion
+
 )
 
-#Ensure PAK has been initialized
-Initialize-PoshArmyKnife
-
-#Read the CSV File into a variable
-$vmlist = Import-Csv $csvfile 
+#test that parameters are correct
+if ((($csvfile) -eq $null) -and ($vcenter -eq $null)){
+    Write-Error -Message "CSV file or vCenter must be provided"
+    Throw
+}
+if(($csvfile -ne $null) -and ($vcenter -ne $null)){
+    Write-Error -Message "If both CSV file and vCenter are specified, vCenter name in CSV will be used"
+    Throw
+}
 
 #Get the user's credentials for vCenter from computer credential store
 $creds = Get-StoredCredential -Target $CredLabel 
 
-#Get the current date/time and place entry into log that a new session has started
-$timestamp = Get-Date 
-
-New-Item $logfile -Force
-
-Add-Content $logfile "#####################################################"
-
-Add-Content $logfile "$timestamp New Session Started"
-
- 
-#Read the vCenters contained in the CSV and dedupe them
-$vcenters = $vmlist | Select-Object -ExpandProperty vCenter -Unique 
-
- 
-#Log into each vCenter included in the CSV file
-foreach ($vcenter in $vcenters) 
-
-    {
-
-    $timestamp = Get-Date #Get the current date/time and place entry into log that the script is connecting to each vCenter
-
-    $message = "$timestamp Connecting to $vcenter"
-
-    Write-Host $message
-
-    Add-Content $logfile  $message
-
-    
-
-    Connect-VIServer $vcenter -Credential $creds #Connect to the vCenter using the credentials provided at first run
-
-    
-
-    Write-Host `n
-
+#Load all VMs or Read the CSV File into an array
+If ($csvfile) {
+    $vmlist = Import-Csv $csvfile 
+    #Read the vCenters contained in the CSV and dedupe them
+    $vcenters = $vmlist | Select-Object -ExpandProperty vCenter -Unique -ErrorAction Stop 
+    $vmlist = $vmlist | Select-Object -ExpandProperty VM -Unique -ErrorAction Stop
+    #Log into each vCenter included in the CSV file
+    foreach ($vcenter in $vcenters) {
+    Write-PSFMessage -Level Output -Message "Connecting to $vcenter"
+    Connect-VIServer -Server $vcenter -Credential $creds -ErrorAction Stop #Connect to the vCenter using the credentials provided at first run
+    }
+}
+else {
+    Connect-VIServer -Server $vcenter -Credential $creds -ErrorAction Stop
+    $vmlist=Get-VM | Select-Object -ExpandProperty Name
     }
 
+Write-PSFMessage -Level Output -Message "New Session Started"
+
+
+
  
-#Remove snapshots for each VM in the CSV
-foreach ($vm in $vmlist)
-
-    {
-
-    #Load the virtual machine object
-    $vm = get-VM -Name $vm.VM 
-
+#Remove snapshots for each VM
+foreach ($vmname in $vmlist){
+    $vm=Get-VM -Name $vmname
     #Get the number of snapshots for the VM
-    $snapshotcount = $vm | Get-Snapshot | Where-Object {($_.Created -lt (Get-Date).AddDays($grace)) -And ($_.Name -like "*$SnapSuffix")} | Measure-Object 
-
+    $snapshotcount = Get-Snapshot -VM $vm | Where-Object {($_.Created -lt (Get-Date).AddDays($grace)) -And ($_.Name -like "*$SnapSuffix")} | Measure-Object 
     #This line makes it easier to insert the number of snapshots into the log file
     $snapshotcount = $snapshotcount.Count 
-
-    
     #Get the current date/time and place entry into log that the script is going to remove x number of shapshots for the VM
-    $timestamp = Get-Date 
-
-    $message = "$timestamp Removing $snapshotcount Snapshot(s) for VM $vm"
-
-    Write-Host $message
-
-    Add-Content $logfile  $message
-
+    Write-PSFMessage -Level Output -Message "Removing $snapshotcount Snapshot(s) for VM $vm"
     
     #Snapshot removal happens here
-    $vm | Get-Snapshot | Where-Object {($_.Created -lt (Get-Date).AddDays($grace)) -And ($_.Name -like "*$SnapSuffix*")} | Remove-Snapshot -confirm:$false | Out-File $logfile -Append 
-
-    #Get the current date/time and place entry into log that the script has finished removing the VM's snapshot(s)
-    $timestamp = Get-Date 
-
-    Add-Content $logfile "$timestamp Snapshots removed for $vm"
-
+    Get-Snapshot -VM $vm | Where-Object {($_.Created -lt (Get-Date).AddDays($grace)) -And ($_.Name -like "*$SnapSuffix*")} | Remove-Snapshot -confirm:$false
     }
 }
